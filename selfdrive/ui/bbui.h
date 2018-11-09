204b4242f8a140645016737eee7f608eaef0159e
arne182 <arneschwarck@gmail.com>
2018-11-09 21:51:44 +0100

    Fix syntax of snprintf

--- selfdrive/ui/bbui.h
+++ selfdrive/ui/bbui.h
@@ -549,7 +549,7 @@ void bb_ui_draw_measures_left( UIState *s, int bb_x, int bb_y, int bb_w ) {
 		}
 
 		snprintf(val_str, sizeof(val_str), (int)(speedlimit * 3.6 + 0.5));
-		snprintf(uom_str, sizeof(uom_str));
+		snprintf(uom_str, sizeof(uom_str), "");
 
 		bb_h +=bb_ui_draw_measure(s, val_str, uom_str, "Limit", 
 			bb_rx, bb_ry, bb_uom_dx,
