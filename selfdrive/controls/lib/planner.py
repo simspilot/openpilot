234b16b28e4a00c05af4a1e162c8635aaba231e1
arne182 <arneschwarck@gmail.com>
2018-11-09 19:11:08 +0100

    Disable brake due to acceleration

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -64,7 +64,7 @@ def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
 
   a_total_max = interp(v_ego, _A_TOTAL_MAX_BP, _A_TOTAL_MAX_V)
   a_y = v_ego**2 * abs(angle_steers) * CV.DEG_TO_RAD / (CP.steerRatio * CP.wheelbase)
-  a_x_allowed = a_total_max - a_y
+  a_x_allowed = min(0., a_total_max - a_y)
 
   a_target[1] = min(a_target[1], a_x_allowed)
   return a_target
