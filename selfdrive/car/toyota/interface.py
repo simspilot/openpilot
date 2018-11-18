43c5241f2a2bed7640e74d67055339a2da28a28e
simspilot <43895273+simspilot@users.noreply.github.com>
2018-11-17 18:30:52 -0800

    update

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -69,7 +69,7 @@ class CarInterface(object):
     tireStiffnessFront_civic = 192150
     tireStiffnessRear_civic = 202500
 
-    ret.steerKiBP, ret.steerKpBP = [[0.], [0.]]
+    ret.steerKiBP, ret.steerKpBP = [[0.,27.,47.], [0.,27.,47.]] #eric's mods
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
 
     if candidate == CAR.PRIUS:
